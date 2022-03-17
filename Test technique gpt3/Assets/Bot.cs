a5e5ed1612b2dd9c8b3eeefb63f9d6d9eabdaf18
Corentin Delplanque <corentin.dlplq@gmail.com>
2022-03-17 13:17:16 +0100

    add of basics of project
    project with basics

---
+++ Test technique gpt3/Assets/Bot.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Bot : MonoBehaviour
+{
+    [SerializeField] Sprite _positive;
+    [SerializeField] Sprite _neutral;
+    [SerializeField] Sprite _negative;
+
+    [SerializeField] Image imageToDispaly;
+    private void Awake()
+    {
+        if (!_positive || !_negative || !_neutral)
+        {
+            Debug.LogError("Missing one sprite");
+            return;
+        }
+
+        imageToDispaly.sprite = _neutral;
+
+    }
+
+    public void ChangeEmotion(int state)
+    {
+        if (state == 1)
+        {
+            imageToDispaly.sprite = _positive;
+        } else if (state == -1)
+        {
+            imageToDispaly.sprite = _negative;
+        } else
+        {
+            imageToDispaly.sprite = _neutral;
+        }
+    }
+}
---
+++ Test technique gpt3/Assets/ChatBot.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using OpenAI_API;
+using System.Threading.Tasks;
+using UnityEngine.UI;
+
+public class ChatBot : MonoBehaviour 
+{
+    [SerializeField] private string _Apikey;
+    [SerializeField] private Bot bot;
+    private string iBriefing;
+    OpenAIAPI api;
+
+    [SerializeField] private InputField inputField;
+
+    public void Send()
+    {
+        var task = RespondAsync();
+    }
+
+    public async Task RespondAsync()
+    {
+        if (inputField.text == "" || inputField.text == null)
+        {
+            bot.ChangeEmotion(0);
+            return;
+        }
+        iBriefing = "Is the sentence \"" + inputField.text + "\" positive? Answer by yes or no:";
+        api = new OpenAIAPI(_Apikey, new Engine("text-davinci-001"));
+        var result = await api.Completions.CreateCompletionAsync(
+                                                                   iBriefing,
+                                                                   max_tokens: 100,
+                                                                   temperature: 1,
+                                                                   top_p: 0,
+                                                                   frequencyPenalty: 0,
+                                                                   presencePenalty: 0);
+
+        string str = YesOrNoParser(result.ToString());
+        if (str == "Yes")
+        {
+            bot.ChangeEmotion(1);
+        } else if (str == "No")
+        {
+            bot.ChangeEmotion(-1);
+        } else
+        {
+            bot.ChangeEmotion(0);    
+        }
+
+    }
+
+    private  string YesOrNoParser(string str)
+    {
+        if (str == null)
+            return "";
+        return str.Replace("\n", string.Empty);
+    }
+}
